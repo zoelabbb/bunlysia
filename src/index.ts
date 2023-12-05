1f3c32dae85d6cf554f5fe78745d4f640d26dd20
Ryu <zoelabbb@gmail.com>
2023-12-05 14:31:16 +0700

    New Bun Project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
+import { cookie } from '@elysiajs/cookie';
+import { jwt } from '@elysiajs/jwt';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    jwt({
+      name: 'jwt',
+      secret: 'Fischl von Luftschloss Narfidort',
+    })
+  )
+  .use(cookie())
+  .get('/sign/:name', async ({ jwt, cookie, setCookie, params }) => {
+    const token = await jwt.sign(params);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+    setCookie('auth', token, {
+      httpOnly: true,
+      maxAge: 7 * 86400,
+    });
+
+    return `Sign in as ${params.name}, your token is ${cookie.auth}`;
+  })
+  .get('/profile', async ({ jwt, set, cookie: { auth } }) => {
+    try {
+      const profile = await jwt.verify(auth);
+
+      if (!profile) {
+        set.status = 401;
+        return 'Unauthorized';
+      }
+
+      return `Hello ${profile.name}`;
+    } catch (error) {
+      set.status = 500;
+      return 'Internal Server Error';
+    }
+  })
+  .listen(8080);
+
+const test = new Elysia()
+  .use(
+    jwt({
+      name: 'jwt',
+      secret: 'kunikuzushi',
+      exp: '7d',
+    })
+  )
+  .get('/sign/:name', async ({ jwt, params }) => jwt.sign(params));
+
+console.log('Listening on http://localhost:8080');
